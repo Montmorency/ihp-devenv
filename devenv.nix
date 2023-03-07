bfe59af77a507cdf6e3ac738e6912ee9cab362a1
Montmorency <lamberh@tcd.ie>
2023-03-07 22:59:53 +0000

    adding postgres to processes.

--- devenv.nix
+++ devenv.nix
@@ -2,7 +2,7 @@
 
 {
   # https://devenv.sh/basics/
-  env.GREET = "devenv";
+  env.GREET = "Welcome to IHP Environment";
 
   # https://devenv.sh/packages/
   packages = [ pkgs.git ];
@@ -15,14 +15,15 @@
     git --version
   '';
 
-  # https://devenv.sh/languages/
-  # languages.nix.enable = true;
-
   # https://devenv.sh/pre-commit-hooks/
   # pre-commit.hooks.shellcheck.enable = true;
 
   # https://devenv.sh/processes/
     services.postgres.enable = true; 
+  # see https://github.com/NixOS/nixpkgs/blob/master/pkgs/servers/sql/postgresql/packages.nix for full list
+    services.postgres.package = postgresql_13.withPackages (p: [ p.pg_cron p.timescaledb p.pg_partman ]);
+    services.postgres.initdbArgs =  ["-D", "build/db/state", "-k", "./build/db", "-c", "listen_addresses="]
+    services.postgres.initialScript = ''CREATE EXTENSION IF NOT EXISTS "uuid-ossp";''
 
   # See full reference at https://devenv.sh/reference/options/
 }
